c87961ab894a1305af7f0f5dbb8c845fb1641cee
leotse89 <leotse89@gmail.com>
2014-07-11 12:20:20 -0700

    Setup the basic structure

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Pacifica Dental Center</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js'></script>
+
+</head>
+
+<body>
+<nav>
+	<a href='index.php?page=1'><img src='imgs/logo.png' class='logo' alt='logo'/></a>
+    <ul>
+        <a href='index.php?page=1'><li>Home</li></a>
+        <a href='index.php?page=2'><li>About</li></a>
+        <a href='index.php?page=3'><li>Services</li></a>
+        <a href='index.php?page=4'><li>Registration</li></a>
+        <a href='index.php?page=5'><li>Contact</li></a>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include("header.php"); ?>
+
+<?php
+	if (isset($_GET["page"])) {
+        switch ($_GET["page"]) {
+            case 1:
+               	include("home.php");
+            break;
+            
+            case 2:
+               	include("about.php");
+            break;
+            
+            case 3:
+                include("services.php");
+            break;
+            
+            case 4:
+                include("registration.php");
+            break;
+            
+            case 5:
+                include("contact.php");
+            break;
+        }	
+    } else {
+		include("home.php");
+	}
+?>
+
+<?php include("footer.php"); ?>
